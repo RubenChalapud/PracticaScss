06911e3952eb45b4fe9a8ca050354c5bf83d111b
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-15 12:33:21 -0500

    Se realizó el Navegador con estilos.

--- src/Components/Navegacion/Navegacion.js
+++ src/Components/Navegacion/Navegacion.js
@@ -4,14 +4,20 @@ import "./Navegacion.css"
 function Navegacion() {
     return (
       <div className="o-navegacion">
-        <div className="o-nav-icons">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-        </div>
+        <svg width="60" height="20" className="o-nav-icons">
+            <circle cx="5" cy="10" r="3" fill="#FFDD64" />
+            <circle cx="25" cy="10" r="3" fill="#D1FFD5" />
+            <circle cx="45" cy="10" r="3" fill="#FFFFFF" />
+        </svg>
         <div className="o-nav-botones">
-          <button>triangulo</button>
-          <button>cuadrado</button>
+          <button className="o-nav-boton">▲</button>
+          <div className="o-nav-container-boton-select">
+            <svg width="4" height="30" className="o-svg-boton">
+              <rect width="4" height="30" fill="#D1FFD5"/>
+            </svg>
+            <button className="o-nav-boton o-nav-boton-select">∎</button>
+          </div>
+          <button className="o-nav-boton o-nav-boton-mas">+</button>
         </div>       
       </div>
     );
