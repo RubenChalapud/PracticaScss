a30bef336a62aedd8aef24d98c43c9a888e0bd49
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-16 11:11:46 -0500

    Se agregó imagen de perfil y color de fondo.

--- src/Components/Perfil/Perfil.js
+++ src/Components/Perfil/Perfil.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Perfil.css"
+import imgPerfil from "./persona.png"
 
 function Perfil() {
     return (
       <div className="o-perfil">
-        <button>Perfilll</button>
+        <div className="o-perfil-container">
+          <img src={imgPerfil} className="o-img-perfil"></img>
+
+        </div>
         
       </div>
     );
