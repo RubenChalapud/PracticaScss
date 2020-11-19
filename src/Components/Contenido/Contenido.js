474daa5718a2e0c61761179eecc830a4a3647f56
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-19 15:13:22 -0500

    Se agregó tickets en contenidos.

--- src/Components/Contenido/Contenido.js
+++ src/Components/Contenido/Contenido.js
@@ -64,6 +64,10 @@ function Contenidos() {
                 <p>23 Sep, 2020</p>
                 <p>L</p>
                 <h3>Software</h3>
+                <svg viewBox="0 500 670 98">
+                  <path fill="white" fillOpacity= "1" d="M 4 598 L 58 572 L 86 599 L 137 575 L 153 599 L 201 571 L 239 599 L 251 565 L 323 593 L 396 565 L 404 599 L 455 571 L 500 599 L 525 568 L 579 599 L 601 568 L 667 598">
+                  </path>
+                </svg>
               </div>
               <div className="o-card-expenses">
                 <div>
@@ -73,9 +77,9 @@ function Contenidos() {
                 <p>26 Sep, 2020</p>
                 <p>L</p>
                 <h3>Management</h3>
-  
-                <svg viewBox="0 500 700 98"><path fill="white" fillOpacity= "1" d="M 4 598 L 58 572 L 86 599 L 137 575 L 153 599 L 201 571 L 239 599 L 251 565 L 323 593 L 396 565 L 404 599 L 455 571 L 500 599 L 525 568 L 579 599 L 601 568 L 667 598">
-                </path>
+                <svg viewBox="0 500 670 98">
+                  <path fill="white" fillOpacity= "1" d="M 4 598 L 58 572 L 86 599 L 137 575 L 153 599 L 201 571 L 239 599 L 251 565 L 323 593 L 396 565 L 404 599 L 455 571 L 500 599 L 525 568 L 579 599 L 601 568 L 667 598">
+                  </path>
                 </svg>
               </div>
               <div className="o-card-expenses">
@@ -86,6 +90,10 @@ function Contenidos() {
                 <p>22 Sep, 2020</p>
                 <p>L</p>
                 <h3>Support</h3>
+                <svg viewBox="0 500 670 98">
+                  <path fill="white" fillOpacity= "1" d="M 4 598 L 58 572 L 86 599 L 137 575 L 153 599 L 201 571 L 239 599 L 251 565 L 323 593 L 396 565 L 404 599 L 455 571 L 500 599 L 525 568 L 579 599 L 601 568 L 667 598">
+                  </path>
+                </svg>
               </div>
             </div>
 
