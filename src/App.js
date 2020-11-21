45babf67a2a557dbdfd4b91fa10c32b4f7580128
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-21 18:15:17 -0500

    Se organizó App con estilos scss.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import AppStyle from './App.module.scss';
 import Navegacion from "./Components/Navegacion/Navegacion"
 import Perfil from "./Components/Perfil/Perfil"
 import Contenido from "./Components/Contenido/Contenido"
 
 function App() {
   return (
-    <div className="App">
+    <div className={AppStyle.App}>
       <Navegacion></Navegacion>
       <Perfil></Perfil>
       <Contenido></Contenido>    
