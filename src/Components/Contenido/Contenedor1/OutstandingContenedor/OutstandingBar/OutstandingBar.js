c879dccc9a571020a999098d348ed9d2faa19e2f
daniela <daniela.tirado@uao.edu.co>
2020-11-21 23:01:53 -0500

    Barra BRS Cali

--- src/Components/Contenido/Contenedor1/OutstandingContenedor/OutstandingBar/OutstandingBar.js
+++ src/Components/Contenido/Contenedor1/OutstandingContenedor/OutstandingBar/OutstandingBar.js
@@ -3,7 +3,11 @@ import OutStandingBarStyle from './OutstandingBar.module.scss';
 function OutstandingBar() {
     return (
             <div className={OutStandingBarStyle.ooutstandingcontainerbar}>
-                <button></button>
+                <div className={OutStandingBarStyle.outbarracontainer}>
+                    <div className={OutStandingBarStyle.outbarranegra}></div>
+                    <div className={OutStandingBarStyle.outbarragris}></div>
+                </div>
+
                 <h5>$21.3k</h5>
             </div>
     );
