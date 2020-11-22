db8d57ef22af130eb03b55550bab14cfe5729295
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-21 22:46:17 -0500

    Se colocó logo de busqueda.

--- src/Components/Contenido/Contenedor1/SearchIcon/SearchIcon.js
+++ src/Components/Contenido/Contenedor1/SearchIcon/SearchIcon.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SearchIconStyle from './SearchIcon.module.scss';
+import Icon from "../../../../Svgs/search.svg"; 
+
 function SearchIcon() {
     return (
         <div className={SearchIconStyle.ocontainersearchicon}>
-            <p className={SearchIconStyle.osearchicon}>L</p>
+            <img src={Icon} className={SearchIconStyle.osearchicon}></img>
         </div>
     );
 }
