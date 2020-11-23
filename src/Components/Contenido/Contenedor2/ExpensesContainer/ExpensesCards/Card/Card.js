a63cfbd09118f767cfd426f01e693b116fd80cf8
JuanG2 <juan.gomez_rod@uao.edu.co>
2020-11-23 17:45:35 -0500

    Logo fix sad

--- src/Components/Contenido/Contenedor2/ExpensesContainer/ExpensesCards/Card/Card.js
+++ src/Components/Contenido/Contenedor2/ExpensesContainer/ExpensesCards/Card/Card.js
@@ -10,8 +10,12 @@ const StylesMap = {
     blue: CardStyle.ocardexpenses,
 };
 
+const Styleslogo= {
+    normal: CardStyle.ologo,
+    manzana: CardStyle.olomanzana,
+};
 
-function Card({monto, fecha, logo, titulo,color}){
+function Card({monto, fecha, logo, titulo,color,slogo}){
     return(
         <div className={StylesMap[color]}>
             <div className={CardStyle.ocardclipcontainer}>
@@ -19,7 +23,7 @@ function Card({monto, fecha, logo, titulo,color}){
                 <img src={Clip} className={CardStyle.oclip}></img>
             </div>
             <p className={CardStyle.ocardfecha}>{fecha}</p>
-            <img src={logo} className={CardStyle.ologo}></img>
+            <img src={logo} className={Styleslogo[slogo]}></img>
             <h3 className={CardStyle.ocardtitulo}>{titulo}</h3>
             <svg viewBox="0 500 670 98">
                 <path fill="white" fillOpacity= "1" d="M 4 598 L 58 572 L 86 599 L 137 575 L 153 599 L 201 571 L 239 599 L 251 565 L 323 593 L 396 565 L 404 599 L 455 571 L 500 599 L 525 568 L 579 599 L 601 568 L 667 598">
