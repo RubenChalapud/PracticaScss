24ae4ae39b814c15d26edae063e5a9fe00e75787
daniela <daniela.tirado@uao.edu.co>
2020-11-23 16:38:06 -0500

    Por fin yeeeii viva petro

--- src/Components/Contenido/Contenedor3/RecentlyContainer/RecentlyNew/RecentlyInvoice/RecentlyInvoice.js
+++ src/Components/Contenido/Contenedor3/RecentlyContainer/RecentlyNew/RecentlyInvoice/RecentlyInvoice.js
@@ -3,10 +3,10 @@ import NumberContainer from "./NumberContainer/NumberContainer";
 import TotalInvoice from "./TotalInvoice/TotalInvoice";
 import RecentlyInvoiceStyle from "./RecentlyInvoice.module.scss"
 
-function RecentlyInvoice({number, title, date, totalTitle, total}){
+function RecentlyInvoice({color, number, title, date, totalTitle, total}){
     return(
         <div className={RecentlyInvoiceStyle.orecenlyinvoice}>
-            <NumberContainer number={number} title={title} date={date}></NumberContainer>
+            <NumberContainer color={color} number={number} title={title} date={date}></NumberContainer>
             <div className={RecentlyInvoiceStyle.oseparador}></div>
             <TotalInvoice totalTitle={totalTitle} total={total}></TotalInvoice>
         </div>
--- src/Components/Contenido/Contenedor3/RecentlyContainer/RecentlyNew/RecentlyInvoice/TotalInvoice/TotalInvoice.js
+++ src/Components/Contenido/Contenedor3/RecentlyContainer/RecentlyNew/RecentlyInvoice/TotalInvoice/TotalInvoice.js
@@ -4,8 +4,8 @@ import TotalInvoiceStyle from "./TotalInvoice.module.scss"
 function TotalInvoice({totalTitle, total}){
     return(
           <div className={TotalInvoiceStyle.ototalinvoice}>
-            <p>{totalTitle}</p>
-            <p>{total}</p>
+            <p className={TotalInvoiceStyle.otitle}>{totalTitle}</p>
+            <p className={TotalInvoiceStyle.op}>{total}</p>
           </div>
     );
 }
