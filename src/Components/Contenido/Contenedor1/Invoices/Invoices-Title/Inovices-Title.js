35ae63d8512af7842a039074a65a226c0026f905
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-23 10:48:58 -0500

    Se colocó fonts

--- src/Components/Contenido/Contenedor1/Invoices/Invoices-Title/Inovices-Title.js
+++ src/Components/Contenido/Contenedor1/Invoices/Invoices-Title/Inovices-Title.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InovicesTitleStyle from './InvoiceTitle.module.scss';
+
 function InvoicesTitle() {
     return (
         <h4 className={InovicesTitleStyle.otitleinvoices}>Invoices</h4>
--- src/Components/Contenido/Contenedor1/Invoices/InvoicesContainers/InvoicesContainer.js
+++ src/Components/Contenido/Contenedor1/Invoices/InvoicesContainers/InvoicesContainer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import InvoicesContainerStyle from './InvoicesContainer.module.scss';
+
 function OInvoices(props){
     return(
     <div className={InvoicesContainerStyle.oinvoice}>
-        <h5>{props.valor}</h5>
-        <p>{props.name}</p>
+        <h5 className={InvoicesContainerStyle.oinvoiceValor}>{props.valor}</h5>
+        <p className={InvoicesContainerStyle.oinvoiceTitle}>{props.name}</p>
     </div>
     );
 }
